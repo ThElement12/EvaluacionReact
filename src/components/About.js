aaa82d3c8befb009f49aa19478f66c32a58133da
Joseph <20170861@ce.pucmm.edu.do>
2020-03-10 12:17:47 -0400

    changes in the about

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div>
       <h1>About us</h1>
-      <p>The "Image Search Engine" is an image search engine developed in React. This obtains the data by consuming the <a href="http://www.unsplash.com/developers" target="_blank" rel="noopener noreferrer">unsplash.com</a> API</p>
+      <p>The "Image Search Engine" is an search engine developed in React that obtains the data/images by consuming the <a href="http://www.unsplash.com/developers" target="_blank" rel="noopener noreferrer">unsplash.com</a> API</p>
     </div>
   )
 }
